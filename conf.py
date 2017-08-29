a8a21ac1d1c48e7e502ecc2f3b9ff5803db95e17
Eric Cageman <cagedev@gmail.com>
2017-08-29 22:20:35 +0200

    last with runestone, magrate now to sphinx with skulpt extension

--- conf.py
+++ conf.py
@@ -180,7 +180,7 @@ html_title = 'Introductie Programmeren met Python Turtle'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = None
+html_favicon = "turtle_HrK_icon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
